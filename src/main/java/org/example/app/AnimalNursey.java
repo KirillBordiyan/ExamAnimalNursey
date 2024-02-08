5f44486f57d7b836e795a613b3ec6936892ffee0
Kirill Bordiyan <kirillbordiyan@gmail.com>
2024-02-08 20:37:07 +0300

    Exam AnimalNursey start

---
+++ src/main/java/org/example/app/AnimalNursey.java
@@ -0,0 +1,10 @@
+package org.example.app;
+
+import org.example.controller.AnimalNurseyController;
+
+public class AnimalNursey {
+    public static void main(String[] args){
+        AnimalNurseyController anc = new AnimalNurseyController();
+        anc.run();
+    }
+}
